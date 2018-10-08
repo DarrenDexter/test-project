77979d42d64dc58318b2c34a0c59741290b43129
darren.dexter <darren.dexter@tribalgroup.com>
2018-10-08 11:02:56 +0100

    commit

--- deploy.cmd
+++ deploy.cmd
@@ -98,11 +98,34 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
 call :SelectNodeVersion
 
 :: 3. Install npm packages
-IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
-  pushd "%DEPLOYMENT_TARGET%"
+IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
+  pushd "%DEPLOYMENT_SOURCE%"
   call :ExecuteCmd !NPM_CMD! install --production
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
+)
+
+
+:: 3. Install npm packages
+IF EXIST "%DEPLOYMENT_SOURCE%\.angular-cli.json" (
+  pushd "%DEPLOYMENT_SOURCE%"
+  call :ExecuteCmd !NPM_CMD! run build
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
+)
+
+:: 3. Install npm packages
+::IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
+::  pushd "%DEPLOYMENT_TARGET%"
+::  call :ExecuteCmd !NPM_CMD! install --production
+::  IF !ERRORLEVEL! NEQ 0 goto error
+::  popd
+::)
+
+IF /I "%IN_PLACE_DEPLOYMENT" NEQ "1" (
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50
+    -f "%DEPLOYMENT_SOURCE%/dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%"
+  IF !ERRORLEVEL! NEQ 0 goto error
 )
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
